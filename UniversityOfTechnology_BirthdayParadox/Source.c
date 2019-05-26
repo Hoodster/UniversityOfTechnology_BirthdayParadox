79b0d9e68a3f3f69db5537fd61c64a0d61d3a275
Hoodster <kuba.porebski3@gmail.com>
2019-05-26 23:03:15 +0200

    working but returning bad results

--- UniversityOfTechnology_BirthdayParadox/Source.c
+++ UniversityOfTechnology_BirthdayParadox/Source.c
@@ -1,8 +1,8 @@
 #define _CRT_SECURE_NO_WARNINGS
 #define foreach(idxtype, idxpvar, col, colsiz ) idxtype* idxpvar; for( idxpvar=col ; idxpvar < (col + (colsiz)) ; idxpvar++)
 #define arraylen( ary ) ( sizeof(ary)/sizeof(ary[0]) )
-#define DNI 364
-#define PROBY 10
+#define DNI 365
+#define PROBY 30000
 
 #include <stdio.h>
 #include <stdlib.h>
@@ -15,7 +15,7 @@ int generujLosowaLiczbe(int min, int max)
 
 int losujDzien(void)
 {
-	return generujLosowaLiczbe(1, DNI + 1);
+	return generujLosowaLiczbe(1, DNI);
 }
 
 int* przydzielDaty(int size)
@@ -28,7 +28,7 @@ int* przydzielDaty(int size)
 
 int main()
 {
-	size_t n = 1;
+	int n = 1;
 	int sukces = 0;
 	int daty[100];
 	int check = PROBY / 2;
@@ -47,21 +47,27 @@ int main()
 				int size = arraylen(daty);
 				for (int r = 0; r < size; r++)
 				{
-					if (daty[r] == -858993460)
-						goto kontrola;
-					if (daty[r] == *date)
+					if (daty[r] < 1 && daty[r] != -858993460 || daty[r] > 365 && daty[r] != -858993460)
 					{
-						kontrolna++;
+						printf("chuj kurwa");
 					}
+					if (daty[r] == -858993460 || *date == -858993460)
+						goto kontrola;
+
+					if (daty[r] == *date)
+						kontrolna++;
 				}
 			}
 
 		kontrola:
 			if (kontrolna >= 2)
 			{
-				sukces++;
+ 				sukces++;
 			}
 
+		}
+		if (n == 23) {
+			printf("%d", sukces);
 		}
 		sukces = 0;
 		n++;
