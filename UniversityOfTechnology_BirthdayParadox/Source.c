0e6afe62ed330a591989694cd8da0acea93eedac
Hoodster <kuba.porebski3@gmail.com>
2019-05-26 20:17:03 +0200

    Added array len + fight with memory allocation

--- UniversityOfTechnology_BirthdayParadox/Source.c
+++ UniversityOfTechnology_BirthdayParadox/Source.c
@@ -1,15 +1,11 @@
 #define _CRT_SECURE_NO_WARNINGS
-#define for_each(item, array) \
-    for(int keep = 1, \
-            count = 0,\
-            size = sizeof (array) / sizeof *(array); \
-        keep && count != size; \
-        keep = !keep, count++) \
-      for(item = (array) + count; keep; keep = !keep)
+#define foreach(idxtype, idxpvar, col, colsiz ) idxtype* idxpvar; for( idxpvar=col ; idxpvar < (col + (colsiz)) ; idxpvar++)
+#define arraylen( ary ) ( sizeof(ary)/sizeof(ary[0]) )
 #define DNI 364
-#define PROBY 30000
+#define PROBY 5
 
 #include <stdio.h>
+#include <stdlib.h>
 
 int generujLosowaLiczbe(int min, int max)
 {
@@ -21,44 +17,36 @@ int losujDzien(void)
 	return generujLosowaLiczbe(1, DNI + 1);
 }
 
-int* przydzielDaty(int size)
+void przydzielDaty(int size, int **arr)
 {
-	int* r;
-	if (sizeof(r) > 0)
-	{
-		r = realloc(r, sizeof(int));
-	}
-	else
-	{
-		r = malloc(size * sizeof(int));
-	}
-
+	*arr = malloc(size * sizeof(int));
+	printf("%d \n", sizeof(arr));
 	for (int i = 0; i < size; i++)
 	{
-		r[i] = losujDzien();
+		arr[i] = losujDzien();
 	}
-	return r;
 }
 
 int main()
 {
 	int n = 1;
 	int sukces = 0;
+	int* ludzie = (int*)malloc(100 * sizeof(int));
+
 	
 	while (sukces <= (PROBY / 2)) 
 	{
 		for (int i = 0; i < PROBY; i++) 
 		{	
-			int* ludzie = przydzielDaty(n);
+			przydzielDaty(n, &ludzie);
 			int kontrolna = 0;
 
-			for_each(int* date, ludzie) 
+			foreach (int, date, ludzie, arraylen(ludzie)) 
 			{
-				int size = sizeof(ludzie) / sizeof(int);
+				int size = arraylen(ludzie);
 				for (int r = 0; r < size; r++)
 				{
-					printf("%d", r);
-					if (ludzie[r] == *date)
+					if (&ludzie[r] == date)
 					{
 						kontrolna++;
 					}
@@ -69,6 +57,7 @@ int main()
 			{
 				sukces++;
 			}
+			ludzie = NULL;
 		}
 
 		sukces = 0;
